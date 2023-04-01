a505667f9ebfe848820f4258f3e7929b22eb7809
salmandma <salmandwimaulanaakbar96@gmail.com>
2023-04-01 20:38:04 +0700

    refactor js to oop and modul

--- script/index.js
+++ script/index.js
@@ -1,284 +1,130 @@
+import { items, Card } from './card.js';
+import { forms, FormValidator, data } from './formValidation.js';
+
 const overlay = document.querySelector('.overlay');
+const sectionProfile = document.querySelector('.profile');
+const btnCloseForm = document.querySelectorAll('.form__icon');
+const btnSaveForm = document.querySelector('.form__button-save');
 const formEdit = document.querySelector('.form-edit');
 const formAdd = document.querySelector('.form-add');
-const sectionProfile = document.querySelector('.profile');
-const btnSaveForm = document.querySelector('.form__button-save');
 const btnCreateForm = document.querySelector('.form__button-create');
-const btnCloseForm = document.querySelectorAll('.form__icon');
 const inputName = document.querySelector('.form__name');
 const inputJob = document.querySelector('.form__job');
 const valueInputName = document.querySelector('.profile__title');
 const valueInputJob = document.querySelector('.profile__text');
-const container = document.querySelector('.pop-up');
+const popup = document.querySelector('.pop-up');
+const inputTitle = document.querySelector('input[name="inputJudul"]');
 
 function showElement(element) {
-	if (element === 'formEdit') {
-		overlay.classList.add('block');
-		formEdit.classList.add('block');
-		inputName.value = valueInputName.textContent;
-		inputJob.value = valueInputJob.textContent;
-	} else if (element === 'popUp') {
-		const popUpImage = document.querySelector('.pop-up__image');
-		const srcImg = event.target.src;
-		const altImg = event.target.alt;
-		popUpImage.setAttribute('src', srcImg);
-		popUpImage.setAttribute('alt', altImg);
+ if (element === 'formEdit') {
+  overlay.classList.add('block');
+  formEdit.classList.add('block');
+  inputName.value = valueInputName.textContent;
+  inputJob.value = valueInputJob.textContent;
+ } else if (element === 'popUp') {
+  const popUpImage = document.querySelector('.pop-up__image');
+  const srcImg = event.target.src;
+  const altImg = event.target.alt;
+  popUpImage.setAttribute('src', srcImg);
+  popUpImage.setAttribute('alt', altImg);
 
-		const popUpTitle = document.querySelector('.pop-up__title');
-		popUpTitle.textContent = altImg;
+  const popUpTitle = document.querySelector('.pop-up__title');
+  popUpTitle.textContent = altImg;
 
-		overlay.classList.add('block');
-		container.classList.add('block');
+  overlay.classList.add('block');
+  popup.classList.add('block');
 
-		document.addEventListener('keydown', keyDownListener);
-	} else {
-		overlay.classList.add('block');
-		formAdd.classList.add('block');
-	}
+  document.addEventListener('keydown', keyDownListener);
+ } else {
+  overlay.classList.add('block');
+  formAdd.classList.add('block');
+ }
 }
 
 function closeElement(element) {
-	if (element === 'form') {
-		// semua yang punya overlay
-		overlay.classList.remove('block');
+ if (element === 'form') {
+  overlay.classList.remove('block');
 
-		// utnuk button edit profile
-		formEdit.classList.add('close');
+  formEdit.classList.add('close');
 
-		// untuk button tambah card
-		formAdd.classList.add('close');
+  formAdd.classList.add('close');
 
-		// // untuk pop-up image
-		container.classList.add('close');
+  popup.classList.add('close');
 
-		setTimeout(() => {
-			formEdit.classList.remove('close');
-			formEdit.classList.remove('block');
+  setTimeout(() => {
+   formEdit.classList.remove('close');
+   formEdit.classList.remove('block');
 
-			formAdd.classList.remove('close');
-			formAdd.classList.remove('block');
+   formAdd.classList.remove('close');
+   formAdd.classList.remove('block');
 
-			container.classList.remove('close');
-			container.classList.remove('block');
-		}, 300);
-	} else {
-		overlay.classList.remove('block');
-		container.classList.add('close');
+   popup.classList.remove('close');
+   popup.classList.remove('block');
+  }, 300);
+ } else {
+  overlay.classList.remove('block');
+  popup.classList.add('close');
 
-		document.removeEventListener('keydown', keyDownListener);
+  document.removeEventListener('keydown', keyDownListener);
 
-		setTimeout(() => {
-			container.classList.remove('close');
-			container.classList.remove('block');
-		}, 300);
-	}
+  setTimeout(() => {
+   popup.classList.remove('close');
+   popup.classList.remove('block');
+  }, 300);
+ }
 }
 
 function saveForm() {
-	valueInputName.textContent = inputName.value;
-	valueInputJob.textContent = inputJob.value;
+ valueInputName.textContent = inputName.value;
+ valueInputJob.textContent = inputJob.value;
 
-	closeElement('form');
+ closeElement('form');
 }
 
-function showAlertBox(element) {
-	if (element === 'sectionProfile') {
-		const sectionProfile = document.querySelector('.profile');
-		const alertContainer = document.createElement('section');
-		alertContainer.classList.add('alert');
-		const alertText = document.createElement('h3');
-		alertText.classList.add('alert__text');
-		alertText.textContent = `Selamat perubahan data ${valueInputName.textContent} dan ${valueInputJob.textContent} telah berhasil !!!`;
+function showAlertBox(textContent) {
+ const sectionProfile = document.querySelector('.profile');
+ const alertContainer = document.createElement('section');
+ alertContainer.classList.add('alert');
+ const alertText = document.createElement('h3');
+ alertText.classList.add('alert__text');
+ alertText.textContent = textContent;
 
-		alertContainer.append(alertText);
-		sectionProfile.before(alertContainer);
+ alertContainer.append(alertText);
+ sectionProfile.before(alertContainer);
 
-		setTimeout(() => {
-			alertContainer.classList.add('close');
-		}, 3000);
-		setTimeout(() => {
-			alertContainer.remove();
-		}, 4000);
-	} else {
-		const sectionProfile = document.querySelector('.profile');
-		const alertContainer = document.createElement('section');
-		alertContainer.classList.add('alert');
-		const alertText = document.createElement('h3');
-		alertText.classList.add('alert__text');
-		alertText.textContent = `Selamat data berhasil di tambahkan dengan judul ${inputTitle.value}`;
-
-		alertContainer.append(alertText);
-		sectionProfile.before(alertContainer);
-
-		setTimeout(() => {
-			alertContainer.classList.add('close');
-		}, 3000);
-		setTimeout(() => {
-			alertContainer.remove();
-		}, 4000);
-	}
+ setTimeout(() => {
+  alertContainer.classList.add('close');
+ }, 3000);
+ setTimeout(() => {
+  alertContainer.remove();
+ }, 4000);
 }
 
 const keyDownListener = (event) => {
-	eventKeyCode(event);
+ eventKeyCode(event);
 };
 
 function eventKeyCode(event) {
-	const keyCode = 27;
-	if (event.keyCode === keyCode) {
-		closeElement();
-	}
+ const keyCode = 27;
+ if (event.keyCode === keyCode) {
+  closeElement();
+ }
 }
 
-sectionProfile.addEventListener('click', (event) => {
-	if (event.target.classList.contains('profile__edit')) {
-		showElement('formEdit');
-		btnSaveForm.classList.add('form__button_inactive');
-	} else if (event.target.classList.contains('profile__add')) {
-		showElement();
-	} else {
-		return;
-	}
+// from card.js
+items.forEach((item) => {
+ const cardItem = new Card(item.name, item.link);
+ const cardElement = cardItem.generateCard();
+ if (item.name && item.link) {
+  document.querySelector('.card').append(cardElement);
+ }
 });
 
-btnCloseForm.forEach((e) => {
-	e.addEventListener('click', () => {
-		closeElement('form');
-	});
+// from formValidation.js
+forms.forEach((form) => {
+ const validator = new FormValidator(data, form);
+
+ validator.enableValidation();
 });
 
-overlay.addEventListener('click', () => {
-	closeElement('form');
-});
-
-btnSaveForm.addEventListener('click', (event) => {
-	saveForm();
-	event.preventDefault();
-	showAlertBox('sectionProfile');
-});
-
-container.addEventListener('click', (event) => {
-	if (event.target.classList.contains('pop-up__icon')) {
-		closeElement();
-	}
-});
-
-btnCreateForm.addEventListener('click', (event) => {
-	event.preventDefault();
-	addInitialCards();
-	filteredCard();
-	showAlertBox();
-	closeElement('form');
-
-	const form = document.forms['formAdd'];
-	form.reset();
-
-	btnCreateForm.classList.add('form__button_inactive');
-});
-
-// card section
-const initialCards = [
-	{
-		name: 'Lembah Yosemite',
-		link: 'https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg',
-	},
-	{
-		name: 'Danau Louise',
-		link: 'https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg',
-	},
-	{
-		name: 'Pegunungan Gundul',
-		link: 'https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg',
-	},
-	{
-		name: 'Gunung Latemar',
-		link: 'https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg',
-	},
-	{
-		name: 'Taman Nasional Vanoise',
-		link: 'https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg',
-	},
-	{
-		name: 'Lago di Braies',
-		link: 'https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg',
-	},
-];
-
-const inputTitle = document.querySelector('input[name="inputJudul"]');
-const inputUrl = document.querySelector('input[name="inputTautanGambar"]');
-
-function addInitialCards() {
-	const result = {
-		name: inputTitle.value,
-		link: inputUrl.value,
-	};
-	initialCards.unshift(result);
-}
-
-const parentElementCard = document.querySelector('.card');
-const cardTemplate = document.querySelector('#cardTemplate');
-function showInitialCards() {
-	initialCards.forEach((card) => {
-		const newCard = cardTemplate.content.cloneNode(true);
-
-		const cardImage = newCard.querySelector('.card__image');
-		cardImage.setAttribute('src', card.link);
-		cardImage.setAttribute('alt', card.name);
-
-		const cardTitle = newCard.querySelector('.card__title');
-		cardTitle.textContent = card.name;
-		cardTitle.setAttribute('title', card.name);
-
-		parentElementCard.append(newCard);
-	});
-}
-
-showInitialCards();
-
-function filteredCard() {
-	const newInputCard = inputTitle.value;
-	const filterCard = initialCards.filter((card) => card.name === newInputCard);
-
-	const newCardContainer = document.createElement('div');
-	newCardContainer.classList.add('card__item');
-
-	const newCardImage = document.createElement('img');
-	newCardImage.classList.add('card__image');
-	newCardImage.setAttribute('src', filterCard[0].link);
-	newCardImage.setAttribute('alt', filterCard[0].name);
-
-	const newCardBtn = document.createElement('button');
-	newCardBtn.classList.add('card__icon-delete', 'hover-icon');
-
-	const newCardMainText = document.createElement('div');
-	newCardMainText.classList.add('card__main-text');
-
-	const newCardTitle = document.createElement('h3');
-	newCardTitle.classList.add('card__title');
-	newCardTitle.setAttribute('title', filterCard[0].name);
-	newCardTitle.textContent = filterCard[0].name;
-
-	const newCardBtnHeart = document.createElement('button');
-	newCardBtnHeart.classList.add('card__icon', 'hover-icon');
-
-	newCardMainText.append(newCardTitle, newCardBtnHeart);
-
-	newCardContainer.append(newCardImage, newCardBtn, newCardMainText);
-
-	const referenceNode = parentElementCard.firstChild;
-
-	parentElementCard.insertBefore(newCardContainer, referenceNode);
-}
-
-parentElementCard.addEventListener('click', (event) => {
-	if (event.target.classList.contains('card__icon-delete')) {
-		event.target.parentElement.remove();
-	} else if (event.target.classList.contains('card__icon')) {
-		event.target.classList.toggle('toggle');
-	} else if (event.target.classList.contains('card__image')) {
-		showElement('popUp');
-	} else {
-		return;
-	}
-});
-// card section end
+export { overlay, popup, sectionProfile, btnCloseForm, btnSaveForm, btnCreateForm, showElement, showAlertBox, closeElement, saveForm, valueInputJob, valueInputName, inputTitle };
---
+++ script/utils.js
@@ -0,0 +1,52 @@
+import { overlay, popup, sectionProfile, btnCloseForm, btnSaveForm, btnCreateForm, showElement, showAlertBox, closeElement, saveForm, valueInputJob, valueInputName, inputTitle } from './index.js';
+
+import { Card } from './card.js';
+
+const card = new Card();
+
+sectionProfile.addEventListener('click', (event) => {
+ if (event.target.classList.contains('profile__edit')) {
+  showElement('formEdit');
+  btnSaveForm.classList.add('form__button_inactive');
+ } else if (event.target.classList.contains('profile__add')) {
+  showElement();
+ } else {
+  return;
+ }
+});
+
+btnCloseForm.forEach((e) => {
+ e.addEventListener('click', () => {
+  closeElement('form');
+ });
+});
+
+overlay.addEventListener('click', () => {
+ closeElement('form');
+});
+
+btnSaveForm.addEventListener('click', (event) => {
+ saveForm();
+ event.preventDefault();
+ showAlertBox(`Selamat perubahan data ${valueInputName.textContent} dan ${valueInputJob.textContent} telah berhasil !!!`);
+});
+
+popup.addEventListener('click', (event) => {
+ if (event.target.classList.contains('pop-up__icon')) {
+  closeElement();
+ }
+});
+
+btnCreateForm.addEventListener('click', (event) => {
+ console.log('hi');
+ event.preventDefault();
+ card.addInitialCards();
+ card.filteredCard();
+ showAlertBox(`Selamat data berhasil di tambahkan dengan judul ${inputTitle.value}`);
+ closeElement('form');
+
+ const form = document.forms['formAdd'];
+ form.reset();
+
+ btnCreateForm.classList.add('form__button_inactive');
+});
