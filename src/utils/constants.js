6abeffae49f62c79d8e85185bf95f5339c7aeb30
salmandma <salmandwimaulanaakbar96@gmail.com>
2023-05-08 23:17:39 +0700

    benerin penamaan bem part 2

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -19,7 +19,7 @@ const cardContainer = document.querySelector('.card');
 const imagePopupSelector = document.querySelector('.pop-up');
 const editProfilePopupSelector = document.querySelector('.form-edit');
 const addNewCardPopupSelector = document.querySelector('.form-add');
-const popupWithConfirmSelector = document.querySelector('.pop-up-confirm');
+const popupWithConfirmSelector = document.querySelector('.form-confirm-container');
 const popupAvatarSelector = document.querySelector('.form-avatar');
 
 export {
